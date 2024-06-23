abfcf4e88fc4f3bd474a598ef14924befc1f9c87
gidizi <gidizimmer@gmail.com>
2024-06-23 23:26:50 +0300

    feat: etching languages on details

--- src/pages/Repos_By_User/components/RepositoryDetails.tsx
+++ src/pages/Repos_By_User/components/RepositoryDetails.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { RepoDetails } from '../types';
 
-type UserContactInfo = Pick<RepoDetails, 'url' | 'openIssuesCount' | 'languagesUrl'>;
+type RepoAdditionalDetails = Pick<RepoDetails, 'url' | 'openIssuesCount' | 'languagesUrl'>;
 
-const RepositoryDetails: React.FC<UserContactInfo> = ({ url, openIssuesCount, languagesUrl }) => {
+//todo: try to make the accordion details load only if it gets expanded
+const RepositoryDetails: React.FC<RepoAdditionalDetails> = ({ url, openIssuesCount, languagesUrl }) => {
+  const [languages, setLanguages] = useState<string[]>([])
+
+  async function getLanguages(languagesUrl: RepoAdditionalDetails['languagesUrl']) {
+    const res = await fetch(languagesUrl)
+    const languageData = await res.json()
+    setLanguages(Object.keys(languageData))
+    //add error handling
+  }
+
+  useEffect(() => {
+    getLanguages(languagesUrl)
+  }, [languagesUrl])
+
   return (
     <ul>
-      <li>Url:{url}</li>
-      <li>open issues:{openIssuesCount}</li>
-      <li>programing languages:</li>
+      <li>Url: {url}</li>
+      <li>open issues: {openIssuesCount}</li>
+      <li>programing languages: {languages.join(", ")}</li>
     </ul>
   );
 };

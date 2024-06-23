4ddc3af6015584bc90f15091d07cd601660a3d73
gidizi <gidizimmer@gmail.com>
2024-06-23 21:30:20 +0300

    feat: interfaces and mapping

--- src/pages/Repos_By_User/ReposByUser.tsx
+++ src/pages/Repos_By_User/ReposByUser.tsx
@@ -5,28 +5,58 @@ const GithubApiUrl = `https://api.github.com/users/gidizi/repos`
 //todo: consider splitting into basic and additional
 interface RepoDetails {
     name: string;
-    description: string;
+    description: string | null;
     starCount: number;
     forkCount: number;
     url: string; //check type
     openIssuesCount: number; //check type
-    languages: string[];
+    languagesUrl: string;
 }
 
+//todo: another api call to get languages
+interface RepoDTO {
+    name: string;
+    description: string | null;
+    stargazers_count: number;
+    forks_count: number;
+    html_url: string;
+    open_issues_count: number;
+    languages_url: string;
+    [key: string]: any;
+}
+
+const mapDTOToRepoDetails = (reposDTO: RepoDTO[]): RepoDetails[] => {
+    const reposDetails = reposDTO.map(repoDTO => {
+        const { name, description, stargazers_count, forks_count, html_url, open_issues_count, languages_url } = repoDTO
+        return {
+            name,
+            description,
+            starCount: stargazers_count,
+            forkCount: forks_count,
+            url: html_url,
+            openIssuesCount: open_issues_count,
+            languagesUrl: languages_url
+        }
+    })
+    return reposDetails
+}
+
+//todo: take care of pagination
 const ReposByUser: React.FC<any> = () => {
     const [username, setUsername] = useState(null) //todo: make it sync to input with the proper type
-    const [reposData, setReposData] = useState([])
+    const [reposData, setReposData] = useState<RepoDetails[]>([])
 
     async function getReposData() {
-        //add the special header
         const res = await fetch(GithubApiUrl, {
             headers: {
                 "Accept": "application/vnd.github+json",
             },
         })
         console.log("res", res)
-        const listReposForUser = await res.json() as RepoDetails[]
+        const listReposForUser = await res.json() as RepoDTO[]
         console.log(listReposForUser)
+        const reposDetails = mapDTOToRepoDetails(listReposForUser)
+        setReposData(reposDetails)
 
         //add error handling
     }
